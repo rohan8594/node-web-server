93117fb2f073f06c7157e06df1a207bafb8f1ca1
rohan8594 <rohan.8594@gmail.com>
2018-11-17 22:10:42 -0800

    Added request handler

--- handler.js
+++ handler.js
@@ -1,12 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
 const requestHandler = (req, res) => {
 	const { url, method } = req;
 	const { statusCode } = res;
 	console.log(`${method} ${url} ${statusCode}`);
 
-	if (url === '/') {
-		res.write('hello world');
-		res.end();
+	let filePath = '.' + url;
+	if (filePath === './') {
+		filePath = './public_html/index.html';
 	}
+
+	fs.exists(filePath, (exists) => {
+		if (!exists) {
+			res.statusCode = 404;
+			res.end(`File ${filePath} not found!`);
+		}
+
+		const extension = path.extname(filePath);
+		let contentType = 'text/html';
+		switch (extension) {
+			case '.js':
+	            contentType = 'text/javascript';
+	            break;
+	        case '.css':
+	            contentType = 'text/css';
+	            break;
+		}
+
+		fs.readFile(filePath, (error, data) => {
+			if (error) {
+				res.statusCode = 500;
+				res.end('Some server side error occured!');
+			} else {
+				res.writeHead(200, { 'Content-Type': contentType });
+				res.end(data, 'utf-8');
+			}
+		})
+	});
+
+	if (url === '/submit' && method === 'POST') {
+	    	// console.log('Hello World')
+	    	res.write('Success!');
+	    	res.end();
+	    }
 }
 
 module.exports = requestHandler;
